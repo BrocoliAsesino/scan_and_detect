58c5cc0913a80af43d67f528499052afaa114367
Alejandro González Hernández <alejandro.gonzalez@local.eurecat.org@CL-AGONZALEZ.local.eurecat.org>
2026-02-27 14:47:10 +0100

    build: updates dependencies and install paths

--- src/pcl_utils/setup.py
+++ src/pcl_utils/setup.py
@@ -5,13 +5,17 @@ package_name = 'pcl_utils'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test', 'test_scripts', 'resource']),
+    packages=find_packages(exclude=['test', 'resource']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'numpy',
+        'open3d',
+    ],
     zip_safe=True,
     maintainer='alejandro.gonzalez@local.eurecat.org',
     maintainer_email='alejandro.gonzalez@eurecat.org',
@@ -24,6 +28,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+                'plc_filters = test_scripts.plc_filters:main',
         ],
     },
 )
